93172bf2e6c6189b167d123e3377123cc1f8be77
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-12 16:47:45 +0200

    Add project files.

---
+++ Recursion and Backtracking/Recursion and Backtracking/Recursion and Backtracking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fibonacciNum = GetFibonacci(21);
+            Console.WriteLine(fibonacciNum);
+        }
+
+        public static int GetFibonacci(int n)
+        {
+            Console.WriteLine(n);
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
