51b7297072a77b723d3602932b167f291ecd99f4
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-13 17:08:08 +0200

    Solve Recursive Factorial.

---
+++ Recursion and Backtracking/Recursion and Backtracking/RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumber = int.Parse(Console.ReadLine());
+
+            var result = CalculateFactorial(inputNumber);
+
+            Console.WriteLine(result);
+        }
+
+        private static int CalculateFactorial(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
