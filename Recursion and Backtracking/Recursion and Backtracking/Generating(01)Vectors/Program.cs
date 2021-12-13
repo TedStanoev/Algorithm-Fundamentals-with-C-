cc233dcf29bffa059a547fa78c19004d169ba9e9
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-13 18:12:18 +0200

    Solve Recursive Drawing and Generating 1/0 Vectors

---
+++ Recursion and Backtracking/Recursion and Backtracking/Generating(01)Vectors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generating_01_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumberOfVectors = int.Parse(Console.ReadLine());
+            var vector = new int[inputNumberOfVectors];
+
+            GenerateVectors(vector, 0);
+        }
+
+        private static void GenerateVectors(int[] vector, int index)
+        {
+            if (index > vector.Length - 1)
+            {
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+
+            for (int j = 0; j < 2; j++)
+            {
+                vector[index] = j;
+                GenerateVectors(vector, index + 1);
+            }
+        }
+    }
+}
---
+++ Recursion and Backtracking/Recursion and Backtracking/RecursiveDrawing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RecursiveDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumber = int.Parse(Console.ReadLine());
+
+            Draw(inputNumber);
+        }
+
+        private static void Draw(int number)
+        {
+            if (number < 1)
+                return;
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write('*');
+            }
+
+            Console.WriteLine();
+
+            Draw(number - 1);
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write('#');
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
