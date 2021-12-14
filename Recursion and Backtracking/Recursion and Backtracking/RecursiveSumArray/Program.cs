d90d8911ec3887337234d0cf13e49c98be364179
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-14 14:15:29 +0200

    Solve Recursive Sum Array.

---
+++ Recursion and Backtracking/Recursion and Backtracking/RecursiveSumArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveSumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var result = GetResult(0, inputArray);
+
+            Console.WriteLine(result);
+        }
+
+        private static int GetResult(int index, int[] array)
+        {
+            if (index == array.Length - 1)
+                return array[index];
+
+            return array[index] + GetResult(index + 1, array);
+        }
+    }
+}
