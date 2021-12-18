97d327e62d03a0e3236bcf75ae2044eb05bf791f
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-18 13:36:29 +0200

    Solve Variations with repetition.

---
+++ Combinatorial Algorithms/CombinatorialProblems/4.VariationsWithRepetition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.VariationsWithRepetition
+{
+    public class Program
+    {
+        static string[] elements;
+        static string[] variations;
+        public static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split().ToArray();
+
+            var variantsCount = int.Parse(Console.ReadLine());
+
+            variations = new string[variantsCount];
+
+            FindVariation(0);
+        }
+
+        private static void FindVariation(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                FindVariation(index + 1);
+            }
+        }
+    }
+}
