d63ec0aa6421a5c81a337762f1742af57451eb00
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-18 14:04:46 +0200

    Solve N choose K count.

---
+++ Combinatorial Algorithms/CombinatorialProblems/6.CombinationsWithRepetition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _6.CombinationsWithRepetition
+{
+    public class Program
+    {
+        static string[] elements;
+        static string[] combinationSet;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split().ToArray();
+
+            var cellsCount = int.Parse(Console.ReadLine());
+
+            combinationSet = new string[cellsCount];
+
+            GetCombination(0, 0);
+        }
+
+        private static void GetCombination(int index, int elementStartIndex)
+        {
+            if (index >= combinationSet.Length)
+            {
+                var set = string.Join(" ", combinationSet);
+
+                Console.WriteLine(set);
+                return;
+            }
+
+            for (int i = elementStartIndex; i < elements.Length; i++)
+            {
+                combinationSet[index] = elements[i];
+                GetCombination(index + 1, i);
+            }
+        }
+    }
+}
