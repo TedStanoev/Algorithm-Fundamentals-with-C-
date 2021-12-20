083c0a02f30c7c1eec0cbe9051a02950993be29d
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-20 17:52:52 +0200

    Solve Nested Loops.

--- Recursion and Combinatorial problems/RecursionAndCombinatorialProblems/2.NestedLoops/Program.cs
+++ Recursion and Combinatorial problems/RecursionAndCombinatorialProblems/2.NestedLoops/Program.cs
@@ -2,11 +2,31 @@
 
 namespace _2.NestedLoops
 {
-    class Program
+    public class Program
     {
+        static int elementsCount;
+        static int[] elements;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            elementsCount = int.Parse(Console.ReadLine());
+            elements = new int[elementsCount];
+
+            Loop(0);
+        }
+
+        private static void Loop(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+            
+            for (int i = 1; i < elements.Length + 1; i++)
+            {
+                elements[index] = i;
+                Loop(index + 1);
+            }
         }
     }
 }
