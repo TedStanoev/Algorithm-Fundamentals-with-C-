2dde84078b1dc4f71f2c94778e92b2d871783d70
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-21 16:12:14 +0200

    Solve School teams.

---
+++ Recursion and Combinatorial problems/RecursionAndCombinatorialProblems/5.SchoolTeams/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.SchoolTeams
+{
+    public class Program
+    {
+        static int girlsCount = 3;
+        static int boysCount = 2;
+
+        static void Main(string[] args)
+        {
+            var girls = Console.ReadLine().Split(", ").ToArray();
+            var girlsCombination = new string[girlsCount];
+            var girlsCombinationList = new List<string[]>();
+
+            GenerateCombinations(0, 0, girls, girlsCombination, girlsCombinationList);
+
+            var boys = Console.ReadLine().Split(", ").ToArray();
+            var boysCombination = new string[boysCount];
+            var boysCombinationList = new List<string[]>();
+
+            GenerateCombinations(0, 0, boys, boysCombination, boysCombinationList);
+
+            PrintCombinations(girlsCombinationList, boysCombinationList);
+        }
+
+        private static void GenerateCombinations(int index, int start, string[] elements, string[] combs, List<string[]> combsList)
+        {
+            if (index >= combs.Length)
+            {
+                combsList.Add(combs.ToArray());
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                combs[index] = elements[i];
+                GenerateCombinations(index + 1, i + 1, elements, combs, combsList);
+            }
+        }
+
+        private static void PrintCombinations(List<string[]> girlsCombs, List<string[]> boysCombs)
+        {
+            foreach (var girlsCombo in girlsCombs)
+            {
+                var girlsOutput = string.Join(", ", girlsCombo);
+
+                foreach (var boysCombo in boysCombs)
+                {
+                    var boysOutput = string.Join(", ", boysCombo);
+
+                    Console.WriteLine($"{girlsOutput}, {boysOutput}");
+                }
+            }
+        }
+    }
+}
---
+++ Recursion and Combinatorial problems/RecursionAndCombinatorialProblems/CinemaWithVariation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CinemaWithVariation
+{
+    public class Program
+    {
+        static List<string> nonStaticPeople;
+        static string[] people;
+        static bool[] takenSeats;
+
+        static void Main(string[] args)
+        {
+            nonStaticPeople = Console.ReadLine().Split(", ", StringSplitOptions.None).ToList();
+            people = new string[nonStaticPeople.Count];
+            takenSeats = new bool[nonStaticPeople.Count];
+
+            string inputLine;
+
+            while ((inputLine = Console.ReadLine()) != "generate")
+            {
+                var parts = inputLine.Split(" - ").ToArray();
+                var name = parts[0];
+                var position = int.Parse(parts[1]) - 1;
+
+                takenSeats[position] = true;
+                nonStaticPeople.Remove(name);
+                people[position] = name;
+            }
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= nonStaticPeople.Count)
+            {
+                PrintOutput();
+                return;
+            }
+
+            Permute(index + 1);
+
+            for (int i = index + 1; i < nonStaticPeople.Count; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
+            }
+        }
+
+        private static void PrintOutput()
+        {
+            int nonStaticPeopleIndex = 0;
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (takenSeats[i] == false)
+                {
+                    people[i] = nonStaticPeople[nonStaticPeopleIndex++];
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', people));
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = nonStaticPeople[first];
+            nonStaticPeople[first] = nonStaticPeople[second];
+            nonStaticPeople[second] = temp;
+        }
+    }
+}
