1130230206b7a453e436b7d36b7bbc00094e09d1
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-27 17:50:20 +0200

    Solve Selection Sort.

---
+++ Searching-Sorting Algorithms/Searching-Sorting/SelectionSort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SelectionSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SelectionSort(array);
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+
+        private static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                var min = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(array, i, min);
+            }
+        }
+
+        private static void Swap(int[] array, int first, int second)
+        {
+            var temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+}
