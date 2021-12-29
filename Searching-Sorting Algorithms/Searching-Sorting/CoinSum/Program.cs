26dadecff42302b8cfee344dbcc65fd02abfb297
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2021-12-29 15:10:31 +0200

    Solve Coin sum and Set cover.

---
+++ Searching-Sorting Algorithms/Searching-Sorting/CoinSum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CoinSum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = Console.ReadLine()
+                .Split(", ", StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            FindSum(coins, target);
+        }
+
+        private static void FindSum(int[] coins, int target)
+        {
+            var sortedCoins = coins
+                .OrderByDescending(c => c)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            var coinsCount = 0;
+            var coinIndex = 0;
+
+            while (target > 0 && coinIndex < sortedCoins.Count)
+            {
+                var currentCoin = sortedCoins[coinIndex++];
+
+                var count = target / currentCoin;
+
+                if (count > 0)
+                {
+                    coinsCount += count;
+                    target -= count * currentCoin;
+                    sb.AppendLine($"{count} coin(s) with value {currentCoin}");
+                }
+            }
+
+            if (target == 0)
+            {
+                Console.WriteLine($"Number of coins to take: {coinsCount}");
+                Console.WriteLine(sb.ToString().Trim());
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ Searching-Sorting Algorithms/Searching-Sorting/SetCover/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetCover
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var universe = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var numberOfSets = int.Parse(Console.ReadLine());
+            var sets = new List<int[]>();
+
+            for (int i = 0; i < numberOfSets; i++)
+            {
+                var set = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                sets.Add(set);
+            }
+
+            var result = TakeSets(universe, sets);
+
+            Console.WriteLine($"Sets to take ({result.Count}):");
+
+            foreach (var set in result)
+            {
+                Console.WriteLine(string.Join(", ", set));
+            }
+        }
+
+        private static List<int[]> TakeSets(int[] universe, List<int[]> sets)
+        {
+            var selectedSets = new List<int[]>();
+
+            while (universe.Length > 0)
+            {
+                var currentSet = sets
+                    .OrderByDescending(s => s.Count(e => universe.Contains(e)))
+                    .FirstOrDefault();
+
+                selectedSets.Add(currentSet);
+                sets.Remove(currentSet);
+
+                universe = universe
+                    .Where(e => !currentSet.Contains(e))
+                    .ToArray();
+            }
+
+            return selectedSets;
+        }
+    }
+}
