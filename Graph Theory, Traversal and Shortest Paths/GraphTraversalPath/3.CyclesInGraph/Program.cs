4c254568f59583208dfd71fed2ff31c74c969f7d
Teodor Stanoev <t.stanoev@onebitsoftware.net>
2022-01-08 15:23:56 +0200

    Solve Cycles in graph.

---
+++ Graph Theory, Traversal and Shortest Paths/GraphTraversalPath/3.CyclesInGraph/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.CyclesInGraph
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static HashSet<string> visited;
+        private static HashSet<string> cycles;
+        public static void Main(string[] args)
+        {
+            graph = new Dictionary<string, List<string>>();
+            visited = new HashSet<string>();
+            cycles = new HashSet<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var pair = input.Split('-');
+                var from = pair[0];
+                var to = pair[1];
+
+                if (!graph.ContainsKey(from))
+                    graph.Add(from, new List<string>());
+
+                if (!graph.ContainsKey(to))
+                    graph.Add(to, new List<string>());
+
+                graph[from].Add(to);
+            }
+
+            try
+            {
+                foreach (var node in graph.Keys)
+                {
+                    DFS(node);
+                }
+
+                Console.WriteLine("Acyclic: Yes");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Acyclic: No");
+            }
+        }
+
+        private static void DFS(string node)
+        {
+            if (cycles.Contains(node))
+                throw new InvalidOperationException();
+            if (visited.Contains(node))
+                return;
+
+            visited.Add(node);
+            cycles.Add(node);
+
+            foreach (var child in graph[node]) 
+            {
+                DFS(child);
+            }
+
+            cycles.Remove(node);
+        }
+    }
+}
