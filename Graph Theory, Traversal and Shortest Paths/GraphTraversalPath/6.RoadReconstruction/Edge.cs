a7a4542d7d843ad7a721b8960b230836f15e5c94
TedStanoev <tedstanoev@gmail.com>
2022-01-14 11:58:37 +0200

    Move class 'Edge' into new file.

---
+++ Graph Theory, Traversal and Shortest Paths/GraphTraversalPath/6.RoadReconstruction/Edge.cs
@@ -0,0 +1,12 @@
+﻿namespace _6.RoadReconstruction
+{
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public override string ToString()
+            => $"{this.First} {this.Second}";
+    }
+}
--- Graph Theory, Traversal and Shortest Paths/GraphTraversalPath/6.RoadReconstruction/Program.cs
+++ Graph Theory, Traversal and Shortest Paths/GraphTraversalPath/6.RoadReconstruction/Program.cs
@@ -4,15 +4,6 @@ using System.Linq;
 
 namespace _6.RoadReconstruction
 {
-    public class Edge
-    {
-        public int First { get; set; }
-
-        public int Second { get; set; }
-
-        public override string ToString()
-            => $"{this.First} {this.Second}";
-    }
     public class Program
     {
         private static List<int>[] graph;
